9ed66b056a09bc5afb85dde16734f816074018b3
brent <bbuenar@wgu.edu>
2023-12-30 17:40:03 -0800

    added constraints validation

--- src/main/java/com/example/computers/compparts/domain/CustomPC.java
+++ src/main/java/com/example/computers/compparts/domain/CustomPC.java
@@ -1,10 +1,20 @@
 package com.example.computers.compparts.domain;
 
 import java.util.List;
+
+//Bean Validation API
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 import lombok.Data;
 
 @Data
 public class CustomPC {
+    
+    @NotNull
+    @Size(min=5, message="Name must be at least 5 characters long") // constraints
     private String name;
+
+    @NotNull
     private List<ComputerParts> computerpart;
 }
--- src/main/java/com/example/computers/compparts/domain/PCOrder.java
+++ src/main/java/com/example/computers/compparts/domain/PCOrder.java
@@ -1,19 +1,44 @@
 package com.example.computers.compparts.domain;
 
 import java.util.List;
+
+import org.hibernate.validator.constraints.CreditCardNumber;
+
+import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
 import java.util.ArrayList;
 import lombok.Data;
 
 
 @Data
 public class PCOrder {
+
+    @NotBlank(message="Name cannot be empty")
     private String customerName;
+    
+    @NotBlank(message="Stress cannot be empty")
     private String customerStreet;
+    
+    @NotBlank(message="City cannot be empty")
     private String customerCity;
+    
+    @NotBlank(message="State cannot be empty")
     private String customerState;
+
+    @NotBlank(message="Zip Code cannot be empty")
+    @Size(max=5)
     private String customerZip;
+    
+    @CreditCardNumber(message="Not a Valid Credit Card Number")
     private String customerCCNum;
+
+    @Digits(integer=3, fraction=0, message="CVV invalid")
     private String customerCCV;
+
+    @Pattern(regexp="^(0[1-9]|1[0-2])([\\/])([2-9][0-9])$", message="Must be formatted MM/YY")
     private String customerCCExpiration;
 
     private List<CustomPC> pcBuild = new ArrayList<>();
